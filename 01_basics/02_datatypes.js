e9eec586fb615c7a9cc9f61066db06b24a046629
yukta-2k23 <142574390+yukta-2k23@users.noreply.github.com>
2024-02-03 18:06:29 +0000

    datatype container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict" //Always use at the start of file to treat all JS code as newer version
+
+//alert(3+3) //Wont work on nodejs but will work on the browser console window
+
+console.log(3+3); console.log("hello") //will run but reduces code readability and thus not a good practice
+
+
+//original documentation for Js on ts39.es
+
+let name="yukta" //string data type
+let age= 18 //int data type
+let isADULT= true //boolean data type
+
+/* MORE Datatypes
+
